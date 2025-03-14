42629549e585b185dca89cb82038071b99982df5
fedor <fedorepisev@gmail.com>
2025-03-15 02:05:19 +0300

    feat: deploy config

--- src/main/resources/db/migration/V1__init_DB.sql
+++ src/main/resources/db/migration/V1__init_DB.sql
@@ -1,26 +1,26 @@
---CREATE TABLE public.movie (
---	id          SERIAL              PRIMARY KEY  NOT NULL,
---	name        VARCHAR(256)        NOT NULL,
---	genre       VARCHAR(256)        NOT NULL,
---	duration    DECIMAL             NOT NULL CHECK(duration > 0)
---);
---
---
---CREATE TABLE public.customer (
---	id          SERIAL              PRIMARY KEY  NOT NULL,
---	name        VARCHAR(256)        NOT NULL,
---	email       TEXT                NOT NULL	CHECK (email ~ '^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$')
---);
---
---
---CREATE TABLE public.ticket (
---    id              SERIAL              PRIMARY KEY  NOT NULL,
---    movie_id        INTEGER             NOT NULL,
---    customer_id     INTEGER             NOT NULL,
---    session_date    TIMESTAMP           NOT NULL,
---    seat            INTEGER             NOT NULL,
---    price           DECIMAL(10, 2)      NOT NULL CHECK (price >= 0),
---    FOREIGN KEY (movie_id) REFERENCES public.movie(id),
---    FOREIGN KEY (customer_id) REFERENCES public.customer(id)
---);
---
+CREATE TABLE public.movie (
+	id          SERIAL              PRIMARY KEY  NOT NULL,
+	name        VARCHAR(256)        NOT NULL,
+	genre       VARCHAR(256)        NOT NULL,
+	duration    DECIMAL             NOT NULL CHECK(duration > 0)
+);
+
+
+CREATE TABLE public.customer (
+	id          SERIAL              PRIMARY KEY  NOT NULL,
+	name        VARCHAR(256)        NOT NULL,
+	email       TEXT                NOT NULL	CHECK (email ~ '^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$')
+);
+
+
+CREATE TABLE public.ticket (
+    id              SERIAL              PRIMARY KEY  NOT NULL,
+    movie_id        INTEGER             NOT NULL,
+    customer_id     INTEGER             NOT NULL,
+    session_date    TIMESTAMP           NOT NULL,
+    seat            INTEGER             NOT NULL,
+    price           DECIMAL(10, 2)      NOT NULL CHECK (price >= 0),
+    FOREIGN KEY (movie_id) REFERENCES public.movie(id),
+    FOREIGN KEY (customer_id) REFERENCES public.customer(id)
+);
+
